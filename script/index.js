ae264576bac090c92852a8bc93210e92b5ebdbe1
AlexanderLyeshin <Alkonaftikus@yandex.ru>
2022-04-16 15:20:19 +0300

    Проектная номер 4 четвёртая версия

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 const openPopUp = document.getElementById('open__popup')
 const closePopUp = document.querySelector('.popup__close')
 const popup = document.getElementById('popup')
-let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__input_username')
-let jobInput = document.querySelector('.popup__input_job')
-let ProfileUserName = document.querySelector('.profile__user-name')
-let ProfileUserDescription = document.querySelector('.profile__user-description')
+const formElement = document.querySelector('.popup__form')
+const nameInput = document.querySelector('#username')
+const jobInput = document.querySelector('#input-job')
+const ProfileUserName = document.querySelector('.profile__user-name')
+const ProfileUserDescription = document.querySelector('.profile__user-description')
 
 function PopupOpenClose() {
     popup.classList.toggle('open');
